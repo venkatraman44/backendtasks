a0cb4d54fa85b973a4496afb8ac86ac5b8cfe6bb
venkatraman <kmlvenkat@gmail.com>
2023-07-27 16:40:29 +0530

    ssghsg

--- src/Controller/CustomController.php
+++ src/Controller/CustomController.php
@@ -16,10 +16,10 @@ class CustomController extends ControllerBase {
    * Function task1.
    */
   public function task1() {
-    $node=Node::load(1);
-    if($node) {
-      if ($node->getType()==='shapes') {
-        $shape=$node->getTitle();
+    $node = Node::load(1);
+    if ($node) {
+      if ($node->getType() === 'shapes') {
+        $shape = $node->getTitle();
         $color_tid = $node->get('field_color')->entity;
         $term = $color_tid->getName();
         $userid = $color_tid->get('field_gil')->entity;
@@ -28,8 +28,8 @@ class CustomController extends ControllerBase {
           '#markup' => "$shape $term $user",
         ];
         return $build;
-    }
+      }
     }
   }
-}
 
+}
--- src/Controller/CustomControllerTwo.php
+++ src/Controller/CustomControllerTwo.php
@@ -36,4 +36,5 @@ class CustomControllerTwo extends ControllerBase {
       return $build;
     }
   }
+
 }
